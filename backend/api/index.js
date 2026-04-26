e759af1ece54fe59cb86a8ddedbc4d85d7c92e67
Rishi Parmar <rishimparmar19@gmail.com>
2026-04-26 19:22:54 +0530

    fresh setup

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,32 +1,14 @@
-import serverless from "serverless-http";
 import express from "express";
-import cors from "cors";
+import serverless from "serverless-http";
 
 const app = express();
 
-
-app.use(express.json());
-app.use(cors({
-  origin: "*", 
-}));
-
-
 app.get("/", (req, res) => {
-  res.json({ message: "Backend is running on Vercel 🚀" });
+  res.json({ message: "Backend working 🚀" });
 });
-
 
 app.get("/api/test", (req, res) => {
-  res.json({ success: true, message: "API working fine" });
+  res.json({ success: true });
 });
-
-app.post("/api/data", (req, res) => {
-  const data = req.body;
-  res.json({
-    message: "Data received successfully",
-    data: data
-  });
-});
-
 
 export default serverless(app);
